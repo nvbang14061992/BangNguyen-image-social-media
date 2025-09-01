4380917d78b96954faa4d3a93447b5ac7b318950
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-09-01 17:03:37 +0700

    add fram of api for getting users, user

---
+++ src/common/helpers/response.helper.js
@@ -0,0 +1,17 @@
+export const responseSuccess = (data, message, code=200) => {
+    return {
+        status: 'success',
+        statusCode: code,
+        message: message,
+        data: data
+    };
+};
+
+export const responseError = (message = "Internal Server Error", statusCode = 500, stack = null) => {
+    return {
+        status: 'Error',
+        statusCode: statusCode,
+        message: message,
+        stack: stack
+    };
+};
---
+++ src/services/user.service.js
@@ -0,0 +1,17 @@
+export const userService = {
+   create: async function (req) {
+      return `This action create`;
+   },
+
+   findAll: async function (req) {
+      return `This action returns all user`;
+   },
+
+   findOne: async function (req) {
+      return `This action returns a id: ${req.params.id} user`;
+   },
+
+   update: async function (req) {
+      return `This action updates a id: ${req.params.id} user`;
+   }
+};
